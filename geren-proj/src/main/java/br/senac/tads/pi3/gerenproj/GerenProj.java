c1ee698f60a7e8774d5ea7d71bc2f5535edf61e7
william.asilva22 <william.asilva22@CAS555506W1008.senacsp.edu.br>
2019-03-01 22:08:50 -0300

    operação for

---
+++ geren-proj/src/main/java/br/senac/tads/pi3/gerenproj/GerenProj.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senac.tads.pi3.gerenproj;
+
+/**
+ *
+ * @author william.asilva22
+ */
+public class GerenProj {
+    
+    public static void main(String[] args) {
+        System.out.println("teste maven");
+        int i = 0;
+        System.out.println("valor de i++: " + i++);
+        for (int j = 0; j < 10; j++) {
+            System.out.println("valor de j: "+ j);
+        }
+    }
+    
+}
